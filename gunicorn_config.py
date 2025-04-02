252e4b4dd3feb1c898984a086e2f1dfdccc81558
Patrick Kang <pkang@webb.org>
2025-04-01 22:29:21 -0700

    added files for deployment

---
+++ gunicorn_config.py
@@ -0,0 +1,7 @@
+bind = "0.0.0.0:5000"
+workers = 3
+timeout = 120
+accesslog = "/var/log/problem_generator/access.log"
+errorlog = "/var/log/problem_generator/error.log"
+capture_output = True
+loglevel = "info"
